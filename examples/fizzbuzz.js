f487645f68e9b083d9a0228db3fe8873303046a1
Liam McLennan <liam.mclennan@gmail.com>
2011-06-02 21:16:28 +1000

    added fizzbuzz.js example

---
+++ examples/fizzbuzz.js
@@ -0,0 +1,19 @@
+function map(value) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    return "fizzbuzz";
+  }
+  if (i % 3 === 0) {
+    return "fizz";
+  }
+  if (i % 5 === 0) {
+    return "buzz";
+  }
+  return i;
+}
+
+
+for (var i = 1; i <= 100; i++)
+{
+  console.log(map(i));
+}
+
